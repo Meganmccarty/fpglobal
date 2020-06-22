cbdf24b8d7776131c0d5ebe36760b51e610c4476
Megan McCarty <megan_mccarty@hotmail.com>
2020-06-22 13:15:30 -0400

    Added models for news app

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from modelcluster.fields import ParentalKey
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
+
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+
+class NewsIndexPage(Page):
+    pass
+
+    def get_context(self, request):
+        # Update context to include only published news, ordered by reverse-chron
+        context = super().get_context(request)
+        newspages = self.get_children().live().order_by('-first_published_at')
+        context['newspages'] = newspages
+        return context
+
+class NewsPageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        'NewsPage',
+        related_name='tagged_items',
+        on_delete=models.CASCADE
+    )
+
+class NewsPage(Page):
+    date = models.DateField("Post date")
+    author = models.CharField(max_length=250, default='', null=True, blank=True)
+    body = RichTextField(blank=True)
+    tags = ClusterTaggableManager(through=NewsPageTag, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel([
+            FieldPanel('author'),
+            FieldPanel('date'),
+            FieldPanel('tags'),
+        ], heading="Post information"),
+        FieldPanel('body'),
+        InlinePanel('gallery_images', label="Gallery images"),
+    ]
+
+    def main_image(self):
+        gallery_item = self.gallery_images.first()
+        if gallery_item:
+            return gallery_item.image
+        else:
+            return None
+
+class NewsPageGalleryImage(Orderable):
+    page = ParentalKey(NewsPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+    )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+class NewsTagIndexPage(Page):
+
+    def get_context(self, request):
+
+        # Filter by tag
+        tag = request.GET.get('tag')
+        newspages = NewsPage.objects.filter(tags__name=tag)
+
+        # Update template context
+        context = super().get_context(request)
+        context['newspages'] = newspages
+        return context
