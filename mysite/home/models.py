7c0446adf187d6c8d08c5625b1d17c4e2053d2f0
Megan McCarty <megan_mccarty@hotmail.com>
2020-06-25 07:34:30 -0400

    Added contact form model and templates

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -4,7 +4,8 @@ from modelcluster.fields import ParentalKey
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField
-from wagtail.admin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
+from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
 
 class HomePage(Page):
@@ -30,3 +31,25 @@ class NewsPageGalleryImage(Orderable):
     panels = [
         FieldPanel('expertise_item'),
     ]
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', related_name='custom_form_fields')
+
+class FormPage(AbstractEmailForm):
+    
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        InlinePanel('custom_form_fields', label='Form fields'),
+        FieldPanel('thank_you_text', classname='null'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email Notification Config"),
+    ]
+
+    def get_form_fields(self):
+        return self.custom_form_fields.all()
