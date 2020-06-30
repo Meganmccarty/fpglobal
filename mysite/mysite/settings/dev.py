205cdbd3f9bfdb99048c212b68ec72a75f2b4dd8
Megan McCarty <megan_mccarty@hotmail.com>
2020-06-30 13:24:22 -0400

    Added email settings, put sensitive data in env

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -7,10 +7,16 @@ DEBUG = True
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['127.0.0.1'] 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+FROM_EMAIL = os.getenv("FROM_EMAIL")
 
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_EMAIL")
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 try:
     from .local import *
